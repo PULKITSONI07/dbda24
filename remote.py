939c6dd5dd81305380425f90958af16a20815069
PULKITSONI07 <82874283+PULKITSONI07@users.noreply.github.com>
2024-09-23 17:42:04 +0530

    remote.py cleaned

--- remote.py
+++ remote.py
@@ -1,6 +1,8 @@
 This is added by some other developer
 This is added remotely .... line 2
 This is added remotely .... line 3
+This is added locally... line 1
+This is local... line 2.
 
 
 
@@ -9,3 +11,11 @@ This is added remotely .... line 3
 
 This is remote file .... line 10
 This is remote file .... line 11
+
+
+
+
+
+
+
+This is added locally...... line 11
